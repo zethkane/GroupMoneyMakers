264b6920472433a82d66193882851d0f9c8cc2fc
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-04-02 16:38:55 -0400

    should be working well enough

--- src/main/Java/models/Checking.java
+++ src/main/Java/models/Checking.java
@@ -1,4 +1,25 @@
 package models;
 
-public class Checking {
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("checking")
+public class Checking extends Account{
+
+    private final int MAX_NUM_OF_MONTHLY_WITHDRAWS = 3;
+    private int numOfWithdraws;
+
+    public Checking(){
+        super();
+        this.numOfWithdraws = 0;
+    }
+
+    public int getNumOfWithdraws() {
+        return numOfWithdraws;
+    }
+
+    public void setNumOfWithdraws(int numOfWithdraws) {
+        this.numOfWithdraws = numOfWithdraws;
+    }
 }
